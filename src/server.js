522662541155ca7f30504d76062ce6ba367a7cab
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-05-24 23:59:23 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 // req,res는 express에서 나온것
 const handleHome = (req, res) => {
   return res.end(); // res.end(); = res를 종료 시킴 res.send("~~"); = ()안에 있는 것을 보냄
 };
-const handleLogin = (req, res) => {
-  return res.send("login");
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+app.get("/", logger, handleHome);
 
 const handelListening = () =>
   console.log(`Server listening on http://loaclhost:${PORT}`);
