fb92dfd73f8174f1b5a1b92f41a90701046de69c
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-06-02 16:48:00 +0900

    db & edit

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { see, edit } from "../controllers/videoController";
+import { getEdit, watch, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see); // :~ >> 변수 들어간다고 express한테 말해줌, //d이 아이디를 숫자만 받게 함
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch); // :~ >> 변수 들어간다고 express한테 말해줌, //d이 아이디를 숫자만 받게 함
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
