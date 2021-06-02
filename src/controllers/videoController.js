69ca119bdb38afa9eebe5a900207841ff8498548
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-06-02 14:17:50 +0900

    iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,7 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" }); // home.pug를 렌더링함
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
