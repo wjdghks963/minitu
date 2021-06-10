5344366853583cdbdaa0d90c2f71d1c00cef4a72
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-06-10 23:28:27 +0900

    404

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,9 @@ import { getEdit, watch, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch); // :~ >> 변수 들어간다고 express한테 말해줌, //d이 아이디를 숫자만 받게 함
-videoRouter.route("/:id(\\d+)").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.get("/:id([0-9a-f]{24})", watch); // :~ >> 변수 들어간다고 express한테 말해줌, //d이 아이디를 숫자만 받게 함
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 export default videoRouter;
+
+// [0-9a-f]{24}  > reqular Expression 0~9와 a~f가 있는 24자와 매치
