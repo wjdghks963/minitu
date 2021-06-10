1affc30bfd001960d8de36e78dbd0226b8b9e214
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-06-10 23:38:05 +0900

    edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,16 +12,21 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  if (video) {
-    return res.render("watch", { pageTitle: video.title, video });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
   }
-  return res.render("404", { pageTitle: "Video not found" });
+  return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
-  const { id } = req.params;
 
-  return res.render("edit", { pageTitle: `Editing ` });
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title} `, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
