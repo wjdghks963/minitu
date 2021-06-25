d46f129f95693fd260013d35969c66fb02e03cb3
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-06-25 23:00:29 +0900

    유저 생성시 중복 없애기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,22 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] }); // $or이 req.body.props 에서 username이나 email 둘 중 하나 true or false로서 값 가져옴
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already exists",
+    });
+  }
+
   await User.create({
     name,
     username,
