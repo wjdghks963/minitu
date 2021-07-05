6090bd6976256d765006c6b2ee1382dd69ddbfd4
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-07-05 20:42:05 +0900

    public, protectied middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,24 @@
 export const localMiddleware = (req, res, next) => {
-  res.session.loggedIn = Boolean(req.session.loggedIn);
+  res.session.loggedIn = Boolean(req.session.loggedIn); // login TURE or FALSE
   req.locals.siteName = "Wetube"; // siteName
   res.locals.loggedInUser = req.session.user || {}; // pug에서 #{loggedInUser}만 써도 req.session.user을 사용 가능함
   next();
+};
+
+// user가 loggedIn됐다면 요청을 계속하게하고 아니면 로그인 페이지로 보냄
+export const protectedMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+// user가 loggedIn됐다면 요청을 계속하게하고 아니면 home으로 보냄
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,13 +7,15 @@ import {
   see,
   startGithubLogin,
 } from "../controllers/userController";
+import { protectedMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectedMiddleware, logout);
+userRouter.route("/edit").all(protectedMiddleware).get(getEdit).post(postEdit);
+// all함수는 get,post 등 어떤 http method를 사용하든지 해당 middleware를 사용할 수 있게 해준다.
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
