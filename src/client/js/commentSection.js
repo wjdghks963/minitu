fb75ac4d27ea902e32232dad0ecf3018e4f4fe61
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-08-11 22:51:26 +0900

    add id to comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -4,14 +4,17 @@ const { async } = require("regenerator-runtime");
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
+  newComment.dataset.id = id;
   newComment.className = "video__comment";
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
   const span = document.createElement("i");
   span.innerText = `${text}`;
+  const span2 = document.createElement("i");
+  span2.innerText = "❌";
   newComment.appendChild(icon);
   newComment.appendChild(span); // append는 밑에 달고
   videoComments.prepend(newComment); // prepend는 element를 밑이 아니라 위에 추가시켜줌
@@ -25,16 +28,18 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
-  textarea.value = ""; // 댓글을 post 후 textarea를 빈칸으로 만든다.
+
   if (status === 201) {
-    addComment(text);
+    textarea.value = ""; // 댓글을 post 후 textarea를 빈칸으로 만든다.
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
   }
 };
 if (form) {
