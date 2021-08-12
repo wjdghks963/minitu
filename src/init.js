12b04be129fbfaa1381c1c7e2c6bd4645d3c9db9
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-08-12 21:10:19 +0900

    babel-cil    babel 없이 node가 이해 할 수 있는 js 코드로 바꿔 준다

--- src/init.js
+++ src/init.js
@@ -1,8 +1,10 @@
+import "regenerator-runtime";
 import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
 import app from "./server";
+
 const PORT = 4000;
 
 const handleListening = () =>
